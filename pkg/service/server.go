0a044e555652d0da8ce09e71e564efe07f9decf9
Nihla Akram <nihla@wso2.com>
2019-08-31 12:21:13 +0530

    Add init server

--- pkg/service/server.go
+++ pkg/service/server.go
@@ -1,9 +1,22 @@
 package service
 
 import ("database/sql"
+	"fmt"
 	"github.com/gorilla/mux")
 
 type Server struct {
 	Router *mux.Router
 	DBCon  *sql.DB
+}
+
+func (service *Server) Init(dbUser, dbPass, dbName, hostname, mysqlPort string) {
+
+	var err error
+	service.DBCon, err = sql.Open("mysql", dbUser+":"+dbPass+"@tcp("+hostname+":"+mysqlPort+")/"+dbName)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	service.Router = mux.NewRouter()
+
 }
