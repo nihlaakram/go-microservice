ba9d15c996423630a2d6a711e390937105409aff
Nihla Akram <nihla@wso2.com>
2019-08-31 12:42:08 +0530

    Implement add articles

--- pkg/model/articles.go
+++ pkg/model/articles.go
@@ -1,8 +1,24 @@
 package model
 
+import "database/sql"
+
 type Article struct {
 	Id      int64  `json:"id"`
 	Title   string `json:"title"`
 	Content string `json:"content"`
 	Author  string `json:"author"`
+}
+
+func (article *Article) AddArticle(db *sql.DB) error {
+	if res, err := db.Exec("INSERT INTO articles VALUES(?, ?, ?, ?)", 0, article.Title, article.Content, article.Author); err != nil {
+		return err
+	} else {
+		id, err := res.LastInsertId()
+		if err != nil {
+			return err
+		}
+		article.Id = id
+	}
+	return nil
+
 }
--- pkg/service/server.go
+++ pkg/service/server.go
@@ -1,7 +1,10 @@
 package service
 
 import ("database/sql"
+	"encoding/json"
 	"fmt"
+	"github.com/nihlaakram/go-microservice/pkg/model"
+	"github.com/nihlaakram/go-microservice/pkg/util"
 	"log"
 	"github.com/gorilla/mux"
 	"net/http"
@@ -34,7 +37,36 @@ func (service *Server) Start(port int) {
 }
 
 func (service *Server) initResource() {
-	service.Router.HandleFunc("/articles", nil).Methods(http.MethodPost)
+	service.Router.HandleFunc("/articles", service.addArticle).Methods(http.MethodPost)
 	service.Router.HandleFunc("/articles", nil).Methods(http.MethodGet)
 	service.Router.HandleFunc("/articles/{id:[0-9]+}", nil).Methods(http.MethodGet)
+}
+
+func (service *Server) addArticle(w http.ResponseWriter, r *http.Request) {
+	var article model.Article
+	body := json.NewDecoder(r.Body)
+	if err := body.Decode(&article); err != nil {
+		failureResponse(w, http.StatusBadRequest, err.Error())
+		return
+	}
+	defer r.Body.Close()
+
+	if err := article.AddArticle(service.DBCon); err != nil {
+		failureResponse(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	writeResponse(w, http.StatusCreated, util.SuccessMsg, article.Id)
+}
+
+func failureResponse(w http.ResponseWriter, code int, message string) {
+	writeResponse(w, code, message, nil)
+}
+
+func writeResponse(w http.ResponseWriter, code int, message string, data interface{}) {
+	response := model.Response{code, message, data}
+	payload, _ := json.Marshal(response)
+
+	w.Header().Set(util.ContentType, util.ApplicationJson)
+	w.WriteHeader(code)
+	w.Write(payload)
 }
